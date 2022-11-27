dcfce4196d5923ea13fbe186c0cafb81a42fddb7
Krishna Kumar <krishnak@utexas.edu>
2022-11-27 11:52:57 -0600

    Fix performance issue on fn and grad

--- heat.jl
+++ heat.jl
@@ -4,44 +4,47 @@ using ForwardDiff
 using DelimitedFiles
 using Statistics
 
-ntime_steps = 100000
-# target porosity nt
-nt = 0.4
-# particle size m
-d50 = 0.025*0.001 
-# target_permeability m2
-target_permeability = (1/180)*((nt^3)/((1-nt)^2))*(d50^2)
-println("Target permeability: ", target_permeability, " porosity: ", nt)
+ntime_steps = 50000
+# target porosity
+n = 0.4
 
-function conduction_convection(permeability = 1e-13, nsteps=10000)
-    # box size, m
-    w = h = 1
-
-    # intervals in x-, y- directions, m
-    dx = dy = 0.005
-
+function soil_props(n)
     # Thermal Conductivity, W/m-K
-    lambdaSoil = 1.0 
-    lambdaWater = 0.6 
+    lambda_soil = 1.0 
+    lambda_water = 0.6 
 
     # Specific heat, J/kg-K
-    CpSoil = 8000 
-    CpWater = 4290 
+    cp_soil = 8000 
+    cp_water = 4290 
 
     # Thermal properties of the soil-water medium
-    lambdaMedium = lambdaSoil*(1-nt)+lambdaWater*nt #W/m-K
-    CpMedium = CpSoil*(1-nt) + CpWater*nt #J/kg-K
+    lambda = lambda_soil * (1-n) + lambda_water * n #W/m-K
+    cp = cp_soil * (1 - n) + cp_water * n #J/kg-K
 
-    # rho water kg/m3
-    rhow = 980 
-
-    # rho Soil kg/m3
-    rhoS = 1850 
+    # Densities kg/m3
+    rhow = 980 # water
+    rhoS = 1850 # soil
 
     #Thermal diffusivity
-    alphaWater = lambdaWater/(rhow * CpWater)
-    alphaSoil = lambdaSoil/(rhoS * CpSoil)
-    alphaMedium = lambdaMedium/(rhoS * CpMedium) 
+    alpha_water = lambda_water/(rhow * cp_water)
+    alpha_soil = lambda_soil/(rhoS * cp_soil)
+    alpha = lambda/(rhoS * cp) 
+
+    # particle size m
+    d50 = 0.025*0.001 
+    # permeability m2
+    permeability = (1/180)*((n^3)/((1-n)^2))*(d50^2)
+    
+    return alpha, permeability
+end
+
+function conduction_convection(permeability, alpha, nsteps)
+    # box size, m
+    w = h = 1
+    # intervals in x-, y- directions, m
+    dx = dy = 0.005
+    # Porosity
+    n = 0.45
     
     # Viscosity kg/m-s
     mu = 1.00E-03 
@@ -49,6 +52,9 @@ function conduction_convection(permeability = 1e-13, nsteps=10000)
     # gravity
     g = 9.81 #m/s2
     
+    # density of water
+    rhow = 980 
+
     # Thermal expansion 
     beta = 8.80E-05  
 
@@ -85,15 +91,14 @@ function conduction_convection(permeability = 1e-13, nsteps=10000)
 
     # Copy to u
     temp = deepcopy(t0)
-    convection_factor = convection * dt * (1/(nt*mu)*permeability*g*rhow) / dy
+    convection_factor = convection * dt * (1 / (n * mu) * permeability * g * rhow) / dy
+
     # Iterate
     for k = 1:nsteps
         for i = 2:nx-1
             for j = 2:ny-1
-                # The velocity corresponds to differential density, since we are measuring the differnetial temp,
-                # the rho(1 - beta(T)) is written as rho*(beta*DeltaT) cable
                 temp[i, j] = t0[i, j] +
-                    conduction * dt * alphaSoil * ((t0[i+1, j] - 2 * t0[i,j] + t0[i-1, j])/dy2 + 
+                    conduction * dt * alpha * ((t0[i+1, j] - 2 * t0[i,j] + t0[i-1, j])/dy2 + 
                                             (t0[i, j+1] - 2 * t0[i,j] + t0[i, j-1])/dx2) + 
                     (t0[i-1,j] - t0[i,j]) * convection_factor * (1 - beta * t0[i,j])
             end
@@ -109,53 +114,25 @@ function conduction_convection(permeability = 1e-13, nsteps=10000)
         t0 = copy(temp)
     end
     return temp
-    
 end
 
-target = conduction_convection(target_permeability,ntime_steps)
-println("Compute target completed")
+# Compute targets
+alpha, target_permeability = soil_props(n)
+println("Target permeability: ", target_permeability, " porosity: ", n)
+target = conduction_convection(target_permeability, alpha, ntime_steps)
+println("Compute target completed, norm target temp: ", norm(target))
 
-function heat_transfer(soil_properties)
-    permeability_factor = soil_properties[1]
-    porosity = soil_properties[2]
+function heat_transfer(permeability_factor, porosity, alpha)
     # box size, m
     w = h = 1
-
     # intervals in x-, y- directions, m
     dx = dy = 0.005
-
-    # Thermal Conductivity, W/m-K
-    lambdaSoil = 1.0 
-    lambdaWater = 0.6 
-
-    # Specific heat, J/kg-K
-    CpSoil = 8000 
-    CpWater = 4290 
-
-    # particle size m
-    d50 = 0.025*0.001 
-
-    # Thermal properties of the soil-water medium
-    lambdaMedium = lambdaSoil*(1-porosity)+lambdaWater*porosity #W/m-K
-    CpMedium = CpSoil*(1-porosity) + CpWater*porosity #J/kg-K
-
     # rho water kg/m3
     rhow = 980 
-
-    # rho Soil kg/m3
-    rhoS = 1850 
-
-    #Thermal diffusivity m2/s
-    alphaWater = lambdaWater/(rhow * CpWater)
-    alphaSoil = lambdaSoil/(rhoS * CpSoil)
-    alphaMedium = lambdaMedium/(rhoS * CpMedium) 
-    
     # Viscosity kg/m-s
     mu = 1.00E-03 
-        
     # gravity
     g = 9.81 #m/s2
-
     # Thermal expansion 
     beta = 8.80E-05
 
@@ -180,9 +157,6 @@ function heat_transfer(soil_properties)
     # nsteps
     nsteps = ntime_steps
 
-    # ========================================#
-    #         Compute Heat Transfer           #
-    # ========================================#
     # Compute heat flow based on permeability
     u0 = Tcool * ones(nx, ny) * permeability_factor
     
@@ -197,16 +171,16 @@ function heat_transfer(soil_properties)
 
     # Copy to u
     u = deepcopy(u0)
-    
-    convection_factor = convection * dt * (1/(porosity*mu) * target_permeability* permeability_factor * g * rhow) / dy
+
+    convection_factor = convection * dt * 
+            (1 / (porosity * mu) * target_permeability * permeability_factor * g * rhow) / dy
+
     # Iterate
     for k = 1:nsteps
         for i = 2:nx-1
             for j = 2:ny-1
-                # The velocity corresponds to differential density, since we are measuring the differnetial temp,
-                # the rho(1 - beta(T)) is written as rho*(beta*DeltaT) cable
                 u[i, j] = u0[i, j] +
-                    conduction * dt * alphaSoil * ((u0[i+1, j] - 2 * u0[i,j] + u0[i-1, j])/dy2 + 
+                    conduction * dt * alpha * ((u0[i+1, j] - 2 * u0[i,j] + u0[i-1, j])/dy2 + 
                                                (u0[i, j+1] - 2 * u0[i,j] + u0[i, j-1])/dx2) + 
                     (u0[i-1,j] - u0[i,j]) * convection_factor * (1 - beta * u0[i,j])
             end
@@ -225,40 +199,41 @@ function heat_transfer(soil_properties)
     return norm(u - target) / norm(target)
 end
 
-
 # Newton Raphson iteration for solving the inverse problem.
 permeability_factor = 0.005
 permeability_tolerance = 1e-10
 porosity = 0.45
 porosity_tolerance = 1e-15
 
-# Initialize soil properties
-soil_properties = zeros(2)
-soil_properties[1] = permeability_factor
-soil_properties[2] = porosity
+alpha, permeability = soil_props(porosity)
+# particle size m
+d50 = 0.025 * 0.001
+
+# Partial derivative of heat wrt permeability factor
+∂heat_∂permeability(permeability_factor, porosity, alpha) = 
+    ForwardDiff.derivative(permeability_factor -> heat_transfer(permeability_factor, porosity, alpha), permeability_factor)
 
 # Iterate to update permeability
 for i = 1:50
-    df = ForwardDiff.gradient(heat_transfer, soil_properties)[1]
-    f = heat_transfer(soil_properties)
+    df = ∂heat_∂permeability(permeability_factor, porosity, alpha)
+    f = heat_transfer(permeability_factor, porosity, alpha)
     println(i, " Permeability: ", permeability_factor * target_permeability, " df: ", df, " f: ", f, " h: ", f/df)
     if abs(f) < permeability_tolerance
         break
     end
     global permeability_factor = permeability_factor - f/df
-    global soil_properties[1] = permeability_factor
     
     # Iterate to update porosity
     k = permeability_factor * target_permeability
-    for i =1:10
-        fn = ((d50^2)/180) * ((porosity^3)/(1-porosity)^2) -k
-        dfn = ((d50^2)/180)*(porosity^4 -4*porosity^3+3*porosity^2)/((1-porosity)^4)
+    for i = 1:10
+        fn = ((d50^2)/180) * ((porosity^3) / (1 - porosity)^2) - k
+        dfn = ((d50^2)/180) * (porosity^4 - 4 * porosity^3 + 3 * porosity^2)/((1 - porosity)^4)
         println(i, " fn: ", fn, "\tdfn: ", dfn, " \tporosity:", porosity)
         if abs(fn) < porosity_tolerance
            break
         end
         global porosity = porosity - fn/dfn
-        global soil_properties[2] = porosity
+        global alpha, perm = soil_props(porosity)
     end
 end
 
